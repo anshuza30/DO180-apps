3bccfda849598e0a0d76f24f00f285f58b540996
anshuza30 <167744161+anshuza30@users.noreply.github.com>
2024-05-17 08:58:48 -0400

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is new file. TESTING 1st time " . PHP_VERSION . "\n";
+print "Hello, World! php version is new file. TESTING 1st & 2nd  time " . PHP_VERSION . "\n";
 ?>
